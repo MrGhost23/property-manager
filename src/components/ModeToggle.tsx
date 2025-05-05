795ccb7a03ba885fe5112d8a56262dd02bdb9417
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2025-05-05 07:09:59 +0300

    feat: Add ModeToggle for dark & light mode

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,31 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useTheme } from "@/contexts/ThemeContext";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="outline-none ring-0">
+          {theme === "dark" ? <Sun /> : <Moon />}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
