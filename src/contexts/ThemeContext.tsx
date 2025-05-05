7d62179e8bc10ef89642c71f69430dadd4ce9469
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2025-05-05 07:13:53 +0300

    feat: Add ThemeContext and ThemeProvider

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext,
+  useEffect,
+  useState,
+  ReactNode,
+  useContext,
+} from "react";
+
+type Theme = "dark" | "light";
+
+type ThemeContextType = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark" || savedTheme === "light"
+      ? (savedTheme as Theme)
+      : "light";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return context;
+}
